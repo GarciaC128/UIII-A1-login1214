050c7218a88ddc098361c1b28cfd786b9c5ebcdd
CARLOS_SANTIAGO_GARCIA_MORALES <a.21308051281214@cbtis128.edu.mx>
2024-06-04 04:02:21 +0000

    A

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // Application name
       title: 'Flutter Hello World',
       // Application theme data, you can set the colors for the application as
@@ -17,27 +18,136 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Login(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text("Login Garcia1214", style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.brown,
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.network(
+                'https://raw.githubusercontent.com/GarciaC128/Img_IOS/main/tablas/restaurante1.jpg',
+                height: 200, // adjust the height as needed
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              TextFormField(
+                controller: emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor ingresa tu email';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: passwordController,
+                decoration: InputDecoration(labelText: 'Contraseña'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor ingresa tu contraseña';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.brown),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    if (emailController.text == "csgm1214@gmail.com" &&
+                        passwordController.text == "checolover11") {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => HomePage(
+                                  email: emailController.text,
+                                )),
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Información invalida')),
+                      );
+                    }
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                          content: Text('Por favor llena los campos')),
+                    );
+                  }
+                },
+                child: const Text('Iniciar',
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key, required this.email});
+
+  final String email;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.brown,
+          title: const Text('Página de inicio',
+              style: TextStyle(color: Colors.white)),
+        ),
+        body: Column(
+          children: [
+            Image.network(
+              'https://raw.githubusercontent.com/GarciaC128/Img_IOS/main/tablas/restaurante1.jpg',
+              height: 200, // adjust the height as needed
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            Text(email),
+            Center(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.brown),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Regresar!",
+                    style: TextStyle(color: Colors.white)),
+              ),
+            ),
+          ],
+        ));
   }
 }
